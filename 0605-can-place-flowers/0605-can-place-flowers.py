5937c045e6448b1603c4bce79f9bcfafa2054fea
prashanth kurimisetty <prasanth4168@gmail.com>
2026-08-24 09:41:17 +0530

    Time: 10 ms (48.59%), Space: 19.6 MB (52.29%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,22 +1,15 @@
 class Solution:
     def canPlaceFlowers(self, fl: List[int], n: int) -> bool:
+        temp=[0]+fl+[0]
         c=0
-        for i in range(0,len(fl)):
-            if len(fl)==1 and fl[0]==0:
-                return True if 1>=n else False
-            if i==0:
-                if fl[i]==0 and fl[1]==0:
-                    c+=1
-                    fl[0]=1
-            if i==len(fl)-1:
-
-                if fl[i]==0 and fl[i-1]==0:
-                    c+=1
-                    break
-            if fl[i]==0 and fl[i-1]==0 and fl[i+1]==0:
+        for i in range(1,len(temp)-1):
+            if temp[i]==0 and temp[i-1]==0 and temp[i+1]==0:
                 c+=1
-                fl[i]=1
-        return True if c>=n else False
+                temp[i]=1
+            if c>=n:
+                return True
+        return False
+       
         
 
         
