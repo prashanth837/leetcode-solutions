da161618192436ae3e0a8566801dc2ff9567fa4d
prashanth kurimisetty <prasanth4168@gmail.com>
2026-08-28 20:52:39 +0530

    Time: 0 ms (100%), Space: 19.2 MB (82.85%) - LeetHub

--- 0781-rabbits-in-forest/0781-rabbits-in-forest.py
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.py
@@ -1,21 +1,24 @@
 class Solution:
     def numRabbits(self, ans: List[int]) -> int:
         freq={}
+        li=0
         for i in ans:
+            if i==0:
+                li+=1
+                continue
             if i not in freq:
                 freq[i]=1
             else:
                 freq[i]+=1
-        li=[]
         for i,j in freq.items():
             x=i
             y=j
             while y>0:
                 if y>x+1:
-                    li.append(x+1)
+                    li+=(x+1)
                     y-=(x+1)
                 else:
-                    li.append(x+1)
+                    li+=(x+1)
                     y=0
-        return sum(li)
+        return li
         
